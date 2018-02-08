551d45d23ad95c81857ff212d40980c13509bcd4
Jonathan Shifman <jonathan.shifman@gmail.com>
2018-02-08 20:53:19 +0200

    Intermediate ops

--- src/test/java/main/BasicJava8Test.java
+++ src/test/java/main/BasicJava8Test.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -19,15 +20,37 @@ public class BasicJava8Test {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		strList = Arrays.asList("a", "aa", "aaa", "b", "bb", "bbb", "c", "cc", "ccc");
+		strList = Arrays.asList("a", "b", "c", "aa", "bb", "cc", "aaa", "bbb", "ccc");
 	}
 
 	@Test
-	public void test() {
+	public void testIntermediateOps() {
 		outputCollection = strList.stream()
-		.filter((str) -> str.length() > 2)
-		.collect(Collectors.toList());
-		System.out.println(outputCollection);
+				.map(str -> str + str)
+				.filter(str -> str.length() >= 3)
+				.collect(Collectors.toList());
+				System.out.println(outputCollection);
+
+		Predicate<String> largerThan3Predicate = str -> str.length() >= 3;
+		outputCollection = strList.stream()
+				.map(this::duplicate)
+				.filter(largerThan3Predicate)
+				.sorted()
+				.collect(Collectors.toList());
+				System.out.println(outputCollection);
+	}
+	
+	private String duplicate(String str) {
+		return str + str;
+	}
+	
+	@Test
+	public void testTeminalOps() {
+		
+	}
+	
+	private String concatenateToBeginning(String str1, String str2) {
+		return str2 + str1;
 	}
 
 }
